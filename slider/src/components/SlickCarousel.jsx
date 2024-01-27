63de1c44352c664d0ed2ad755a19c808aab6e643
MLDODSON <paola@dominicansoul.com>
2024-01-26 21:12:28 -0600

    Branch 5
    Branch 5 - Setting up React Slick for the slider.

--- slider/src/components/SlickCarousel.jsx
+++ slider/src/components/SlickCarousel.jsx
@@ -1,6 +1,42 @@
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+import Slider from "react-slick";
+import { LongList } from "../data";
+import { FaQuoteRight } from "react-icons/fa";
+
 const SlickCarousel = () => {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+
     return (
-      <h1>Slick Carousel</h1>
+      <section className="mx-auto mt-[10rem] w-[80vw] max-w-[800px] text-center">
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </section>
     )
   };
   
